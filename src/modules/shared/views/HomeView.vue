e6635223adb2cf0f64ca647bc6dfbc1e000f8e6c
Joan Morales <josmoralesp0307@outlook.es>
2022-12-15 20:19:29 -0500

    Creation of the navigation bar for users

---
+++ src/modules/shared/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <NavbarAuthenticated />
+    <!-- <NavbarUnauthenticated /> -->
+</template>
+
+<script setup lang="ts">
+import NavbarAuthenticated from '../components/NavbarAuthenticated.vue';
+import NavbarUnauthenticated from '../components/NavbarUnauthenticated.vue';
+ 
+</script>
+
+<style scoped>
+
+</style>
