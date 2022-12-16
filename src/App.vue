a9ce6ed753f576979c1a538437d31eedd6209283
Joan Morales <josmoralesp0307@outlook.es>
2022-12-15 20:02:33 -0500

    Initial Configuration

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello Ferrentacar!
+        </h1>
+
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+
+        <button type="button" class="btn-default">
+            Default
+        </button>
+        <button type="button" class="btn-alternative">Alternative</button>
+        <button type="button" class="btn-dark">Dark</button>
+        <button type="button" class="btn-light">Light</button>
+        <button type="button" class="btn-green">Green</button>
+        <button type="button" class="btn-red">Red</button>
+        <button type="button" class="btn-yellow">Yellow</button>
+        <button type="button" class="btn-purple">Purple</button>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+    
+<style scoped>
+
+</style>        
