5761dd5a2d7ad9b6b541ac7daf341af9aabf5beb
Joan Morales <josmoralesp0307@outlook.es>
2022-12-15 20:52:29 -0500

    General button component definition

---
+++ src/modules/shared/components/ButtonComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <button :class="props.typeButton">
+        {{ props.nameButton }}
+    </button>
+</template>
+
+<script setup lang="ts">
+// import '@/assets/styles.css'
+const props = defineProps({
+    nameButton: {
+        type: String,
+        required: true
+    },
+    typeButton: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
